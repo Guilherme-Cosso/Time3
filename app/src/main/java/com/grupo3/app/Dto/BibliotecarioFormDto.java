aaab2adf87ef7847f834b9e6ca03ea2bb5ea5f2e
Jardel Morais <moraiss.jardel@gmail.com>
2022-01-11 15:32:14 -0300

    colocando anotação not blank nos forms

--- app/src/main/java/com/grupo3/app/Dto/BibliotecarioFormDto.java
+++ app/src/main/java/com/grupo3/app/Dto/BibliotecarioFormDto.java
@@ -1,5 +1,7 @@
 package com.grupo3.app.Dto;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BibliotecarioFormDto {
 
-	private Long Id;
+	@NotBlank
 	private String nome;
+	@NotBlank
 	private String email;
+	@NotBlank
 	private String cpf;
+	@NotBlank
 	private String telefone;
 }
